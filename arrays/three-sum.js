f1fa761a6ae05c7d164111e5fb843f6ef03fae00
Kevin Mason <krmason2020@gmail.com>
2022-06-24 22:50:12 -0700

    struggled with 3 sum

---
+++ arrays/three-sum.js
@@ -0,0 +1,41 @@
+//15. 3Sum slow running solution
+const threeSum = (nums) => {
+  let arr = nums.sort((m, n) => m - n);
+  let result = [];
+  const hash = {};
+
+  if (nums.length < 3) {
+    return [];
+  }
+
+  for (let tgtIdx = arr.length - 1; tgtIdx >= 0; tgtIdx--) {
+    let target = arr[tgtIdx];
+
+    let right = arr.length - 1;
+    let left = 0;
+
+    while (left < right) {
+      if (arr[left] + arr[right] === target * -1) {
+        const triplet = [arr[left], arr[right], target].sort((m, n) => m - n);
+
+        if (
+          hash[triplet] === undefined &&
+          left !== tgtIdx &&
+          right !== tgtIdx
+        ) {
+          hash[triplet] = true;
+          result.push(triplet);
+        }
+        right--;
+      } else if (arr[left] + arr[right] < target * -1) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+
+    right = arr.length - 1;
+    left = 0;
+  }
+  return result;
+};
