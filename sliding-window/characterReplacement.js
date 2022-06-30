8354274f15d642b5b3eb042c5e84a4221f8df1b0
Kevin Mason <krmason2020@gmail.com>
2022-06-29 21:39:25 -0700

    struggled with character replace, should have used whiteboard

---
+++ sliding-window/characterReplacement.js
@@ -0,0 +1,39 @@
+const characterReplacement = (s, k) => {
+  const charCounter = {};
+  let total = 0;
+  let left = 0;
+  let right = 0;
+  let maxLen = 0;
+  let maxChar = 0;
+
+  while (right < s.length) {
+    if (charCounter[s[right]] === undefined) {
+      charCounter[s[right]] = 1;
+    } else {
+      charCounter[s[right]]++;
+    }
+    maxChar = Math.max(maxChar, charCounter[s[right]]);
+    total++;
+
+    while (k < total - maxChar) {
+      charCounter[s[left]]--;
+      total--;
+      maxChar = Math.max(maxChar, charCounter[s[left]]);
+      left++;
+    }
+
+    maxLen = Math.max(maxLen, right - left + 1);
+    right++;
+  }
+  return maxLen;
+};
+
+function determineMaxChar(obj) {
+  let max = 0;
+  for (const key in obj) {
+    max = Math.max(max, obj[key]);
+  }
+  return max;
+}
+
+console.log(characterReplacement("AABABBA", 2));
--- sliding-window/permutation-in-string.js
+++
@@ -1,7 +0,0 @@
-// Input: String="oidbcaf", Pattern="abc"
-// Output: true
-// Explanation: The string contains "bca" which is a permutation of the given pattern.
-
-// Input: String="odicf", Pattern="dc"
-// Output: false
-// Explanation: No permutation of the pattern is present in the given string as a substring.
